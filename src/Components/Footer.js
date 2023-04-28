dad3f513d3ccf653249c5449c4c6501866dc2db8
Arun Yallappa Ireshanavar <34903976+arun22ayi@users.noreply.github.com>
2023-04-28 11:53:15 +0530

    Add files via upload

---
+++ src/Components/Footer.js
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <h4 className="p-10 text-center">
+      This site is developed by{" "}
+      <a
+        className="text-orange-400"
+        href="https://www.linkedin.com/in/aryan-chaudhary-133855230/"
+        target="_blank"
+      >
+        Arun Ireshanavar
+      </a>
+    </h4>
+  );
+};
+
+export default Footer;
